6da2a637b88ca8c669af78c1d625f6460c138dd4
duddnddnfl <duddnddnfl@naver.com>
2025-03-23 18:39:12 +0900

    [feat/#96] allergy 관련 예외 코드 수정

--- src/main/java/org/example/recipe_match_backend/global/exception/type/AllergyTypeException.java
+++ src/main/java/org/example/recipe_match_backend/global/exception/type/AllergyTypeException.java
@@ -4,7 +4,7 @@ import org.example.recipe_match_backend.global.exception.BusinessException;
 import org.example.recipe_match_backend.global.exception.ErrorCode;
 
 public class AllergyTypeException extends BusinessException {
-    public AllergyTypeException() {
-        super(ErrorCode.ALLERGY_TYPE_NOT_FOUND);
+    public AllergyTypeException(String allergyType) {
+        super(ErrorCode.valueOf(ErrorCode.ALLERGY_TYPE_NOT_FOUND + allergyType));
     }
 }
