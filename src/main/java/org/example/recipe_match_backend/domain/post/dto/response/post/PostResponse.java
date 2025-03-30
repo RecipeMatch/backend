5c517822e180c0078c4606bc079eee04ae1811bd
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 02:15:37 +0900

    [feat/#102] Post 관련 DTO 구현

--- src/main/java/org/example/recipe_match_backend/domain/post/dto/response/post/PostResponse.java
+++ src/main/java/org/example/recipe_match_backend/domain/post/dto/response/post/PostResponse.java
@@ -1,9 +1,12 @@
 package org.example.recipe_match_backend.domain.post.dto.response.post;
 
+import lombok.Getter;
+import org.example.recipe_match_backend.domain.post.domain.Post;
 import org.example.recipe_match_backend.domain.post.domain.PostComment;
 
 import java.util.List;
 
+@Getter
 public class PostResponse {
 
     private Long postId;
@@ -11,5 +14,12 @@ public class PostResponse {
     private String content;
     private List<PostComment> comments;
 
-
+    public static PostResponse of(Post post) {
+        PostResponse response = new PostResponse();
+        response.postId = post.getId();
+        response.title = post.getTitle();
+        response.content = post.getContent();
+        response.comments = post.getPostComments();
+        return response;
+    }
 }
