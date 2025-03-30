1c4d359801fd8c0d4a0248d8744cc1f29ebf4922
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 04:21:07 +0900

    [feat/#105] 게시물 관련 예외코드 유지보수

--- src/main/java/org/example/recipe_match_backend/global/exception/post/PostNotFoundException.java
+++ src/main/java/org/example/recipe_match_backend/global/exception/post/PostNotFoundException.java
@@ -3,8 +3,9 @@ package org.example.recipe_match_backend.global.exception.post;
 import org.example.recipe_match_backend.global.exception.BusinessException;
 import org.example.recipe_match_backend.global.exception.ErrorCode;
 
-public class PostCommentNotFound extends BusinessException {
-    public PostCommentNotFound() {
-        super(ErrorCode.POST_COMMENT_NOT_FOUND);
+public class PostNotFoundException extends BusinessException {
+    public PostNotFoundException() {
+        super(ErrorCode.POST_NOT_FOUND);
     }
+
 }
