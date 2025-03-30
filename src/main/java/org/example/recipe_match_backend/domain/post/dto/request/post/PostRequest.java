4c0d0cb300a9e01f80b47be541da20c09e88e9af
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 01:28:54 +0900

    [feat/#102] Post 관련 DTO 구현

--- src/main/java/org/example/recipe_match_backend/domain/post/dto/request/post/PostRequest.java
+++ src/main/java/org/example/recipe_match_backend/domain/post/dto/request/post/PostRequest.java
@@ -1,7 +1,10 @@
 package org.example.recipe_match_backend.domain.post.dto.request.post;
 
 public class PostRequest {
-
+    
+    private String uid;     // 사용자 이메일
+    private String title;   // 게시판 제목
+    private String content; // 게시판 내용
 
 
 }
--- src/main/java/org/example/recipe_match_backend/domain/post/dto/response/post/PostResponse.java
+++ src/main/java/org/example/recipe_match_backend/domain/post/dto/response/post/PostResponse.java
@@ -1,6 +1,5 @@
 package org.example.recipe_match_backend.domain.post.dto.response.post;
 
-import jakarta.persistence.Column;
 import org.example.recipe_match_backend.domain.post.domain.PostComment;
 
 import java.util.List;
@@ -8,9 +7,9 @@ import java.util.List;
 public class PostResponse {
 
     private Long postId;
-    private String userUid;
     private String title;
     private String content;
     private List<PostComment> comments;
+
 
 }
