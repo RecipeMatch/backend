69fc8860a4f7023237e026948b25a071a700f553
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 04:23:49 +0900

    [feat/#105] 게시물 댓글 생성 로직 구현

--- src/main/java/org/example/recipe_match_backend/domain/post/service/PostCommentService.java
+++ src/main/java/org/example/recipe_match_backend/domain/post/service/PostCommentService.java
@@ -3,13 +3,16 @@ package org.example.recipe_match_backend.domain.post.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.recipe_match_backend.domain.post.domain.Post;
+import org.example.recipe_match_backend.domain.post.domain.PostComment;
 import org.example.recipe_match_backend.domain.post.dto.request.post_comment.PostCommentCreateRequest;
 import org.example.recipe_match_backend.domain.post.dto.request.post_comment.PostCommentUpdateRequest;
 import org.example.recipe_match_backend.domain.post.dto.response.post_comment.PostCommentResponse;
 import org.example.recipe_match_backend.domain.post.repository.PostCommentRepository;
 import org.example.recipe_match_backend.domain.post.repository.PostRepository;
+import org.example.recipe_match_backend.domain.user.domain.User;
 import org.example.recipe_match_backend.domain.user.repository.UserRepository;
-import org.example.recipe_match_backend.global.exception.post.PostNotFound;
+import org.example.recipe_match_backend.global.exception.post.PostNotFoundException;
+import org.example.recipe_match_backend.global.exception.user.UserNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,7 +30,7 @@ public class PostCommentService {
 
     public List<PostCommentResponse> findAll(Long postId) {
         Post post = postRepository.findById(postId)
-                .orElseThrow(PostNotFound::new);
+                .orElseThrow(PostNotFoundException::new);
 
         return post.getPostComments().stream()
                 .map(PostCommentResponse::from)
@@ -36,7 +39,18 @@ public class PostCommentService {
 
     @Transactional
     public void create(Long postId, PostCommentCreateRequest request) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(PostNotFoundException::new);
+        User user = userRepository.findByUid(request.getUid())
+                .orElseThrow(UserNotFoundException::new);
 
+        PostComment comment = PostComment.builder()
+                .post(post)
+                .user(user)
+                .content(request.getContent())
+                .build();
+
+        postCommentRepository.save(comment);
     }
 
     @Transactional
