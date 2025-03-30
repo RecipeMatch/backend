946df309e57e094b2cb5309be941acd315a973d1
duddnddnfl <duddnddnfl@naver.com>
2025-03-31 00:53:50 +0900

    [feat/#96] PostComment 관련 기본틀 구현

---
+++ src/main/java/org/example/recipe_match_backend/domain/post/controller/PostCommentController.java
@@ -0,0 +1,14 @@
+package org.example.recipe_match_backend.domain.post.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+public class PostCommentController {
+
+
+
+}
---
+++ src/main/java/org/example/recipe_match_backend/domain/post/service/PostCommentService.java
@@ -0,0 +1,16 @@
+package org.example.recipe_match_backend.domain.post.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PostCommentService {
+
+
+
+}
