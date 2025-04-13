680bc83742b125e77a2fec0d1d1b3f161ffcced6
duddnddnfl <duddnddnfl@naver.com>
2025-04-13 21:44:12 +0900

    [feat/#132] 상품 응답 DTO 구현

--- src/main/java/org/example/recipe_match_backend/global/coupang/dto/response/ProductRecommendationResponse.java
+++ src/main/java/org/example/recipe_match_backend/global/coupang/dto/response/ProductRecommendationResponse.java
@@ -1,14 +1,14 @@
 package org.example.recipe_match_backend.global.coupang.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import org.example.recipe_match_backend.global.coupang.dto.ProductDto;
 
 import java.util.List;
 
 @Getter
-@Setter
+@Builder
 @AllArgsConstructor
 public class ProductRecommendationResponse {
     private boolean hasMissing;
