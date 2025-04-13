f13497ddf510ead55cb9fb498430b79fe632bc4a
duddnddnfl <duddnddnfl@naver.com>
2025-04-13 21:44:24 +0900

    [feat/#132] 상품 정보 DTO 구현

--- src/main/java/org/example/recipe_match_backend/global/coupang/dto/ProductDto.java
+++ src/main/java/org/example/recipe_match_backend/global/coupang/dto/ProductDto.java
@@ -1,11 +1,12 @@
 package org.example.recipe_match_backend.global.coupang.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 @AllArgsConstructor
 public class ProductDto {
     private String name;
