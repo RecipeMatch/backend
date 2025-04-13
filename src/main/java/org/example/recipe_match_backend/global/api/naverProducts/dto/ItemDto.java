e44896746cb94a3cb035ea2ede78d82f9b2e4faa
duddnddnfl <duddnddnfl@naver.com>
2025-04-14 00:15:57 +0900

    [feat/#132] 네이버 쇼핑 검색 API DTO 구현

--- src/main/java/org/example/recipe_match_backend/global/api/naverProducts/dto/ItemDto.java
+++ src/main/java/org/example/recipe_match_backend/global/api/naverProducts/dto/ItemDto.java
@@ -1,8 +1,10 @@
 package org.example.recipe_match_backend.global.api.naverProducts.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ItemDto {
     private String title;
     private String link;
