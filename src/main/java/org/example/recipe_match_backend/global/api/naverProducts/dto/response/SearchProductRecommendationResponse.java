c3db95055340d0e20a4403aaaf4f86ee91e00c1b
duddnddnfl <duddnddnfl@naver.com>
2025-04-14 23:54:42 +0900

    [feat/#134] 사용자가 검색한 상품 추천 DTO 작성

---
+++ src/main/java/org/example/recipe_match_backend/global/api/naverProducts/dto/response/SearchProductRecommendationResponse.java
@@ -0,0 +1,15 @@
+package org.example.recipe_match_backend.global.api.naverProducts.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.example.recipe_match_backend.global.api.naverProducts.dto.ProductDto;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SearchProductRecommendationResponse {
+    private List<ProductDto> products;
+}
